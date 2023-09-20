d801341b98eac63949f7c31fe15efacd819e633f
Tal Segalov <tal.segalov@anaplan.com>
2023-09-20 16:52:10 +0300

    Added streamlit app

---
+++ WordCipher/word_cipher_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from word_cipher_gen import WordCipherGen
+
+st.title("שאלות")
+q_cols = st.columns([0.3, 0.7])
+with q_cols[1]:
+    st.write("שאלה")
+    questions = [st.text_input("", key=f"q{i}") for i in range(6)]
+
+with q_cols[0]:
+    st.write("תשובה")
+    answers = [st.text_input("", key=f"a{i}") for i in range(6)]
+
+st.title("הסוד")
+s_cols = st.columns([0.3, 0.7])
+with s_cols[1]:
+    st.write("השאלה לסוד")
+    secret_q = st.text_input("", key="sq")
+
+with s_cols[0]:
+    st.write("התשובה")
+    secret_ans = st.text_input("", key="si")
+
+if st.button("Go!"):
+    sentences = [(questions[i], answers[i]) for i in range(len(questions)) if (questions[i] is not None) and (len(questions[i]) > 0)]
+    secret = (secret_q, secret_ans)
+
+    word_cipher = WordCipherGen()
+    word_cipher.create_doc(sentences, secret)
+    word_cipher.save_doc("/tmp/worksheet.docx")
+
+    with open("/tmp/worksheet.docx", "rb") as fp:
+        st.download_button("Download", data=fp, file_name="worksheet.docx")
--- WordCipher/word_cipher_gen.py
+++ WordCipher/word_cipher_gen.py
@@ -18,113 +18,131 @@ SQUARE_SIZE = 28
 LETTER_16_WIDTH = 0.2
 MARGIN_WIDTH = 0.9
 
-def encrypt_text(solutions, secret):
-    """
-    Creates an encryption for the solutions and secret text, replacing each unique char with one of our unicodes:
-    :param solutions: the list of strings that are the solutions
-    :param secret: The secret text we want to discover last
-    :return: dict with keys: {"solutions": [list of encrypted solutions], "secret": encrypted secret string, "key": key}
-    """
-    unique_chars = set([l for l in "".join(solutions)])
-    assert all(l in unique_chars for l in secret), Exception("Not all chars in secret are in the solutions")
+class WordCipherGen:
+    def __init__(self):
+        """
+        Init the class
+        """
+        self.mapping_dict = None
+        self.solutions_enc = None
+        self.secret_enc = None
+        self.doc = None
 
-    # Now create a random order of the unicodes:
-    unicode_ord = np.random.choice(len(unicodes_list), len(unique_chars), replace=False)
-    mapping_dict = {c:k for c,k in zip(unique_chars, unicode_ord)}
+    def _new_doc(self):
+        # Create a new doc:
+        self.doc = Document()
 
-    # Now "encrypt" the solutions and the secret:
-    res = {"key": mapping_dict,
-           "solutions": [[unicodes_list[mapping_dict[c]] for c in sol] for sol in solutions],
-           "secret": [unicodes_list[mapping_dict[c]] for c in secret]}
-    return res
+    def encrypt_text(self, solutions, secret):
+        """
+        Creates an encryption for the solutions and secret text, replacing each unique char with one of our unicodes
+        Sets the mapping key in self.mapping_dict, the solutions encoded in self.solutions_enc and the secret encoded in self.secret_enc
+        :param solutions: the list of strings that are the solutions
+        :param secret: The secret text we want to discover last
+        :return:
+        """
+        unique_chars = set([l for l in "".join(solutions)])
+        assert all(l in unique_chars for l in secret), Exception("Not all chars in secret are in the solutions")
 
-def _set_font_size(paragraphs, font_size=20):
-    for paragraph in paragraphs:
-        for run in paragraph.runs:
-            run.font.size = Pt(font_size)  # Set font size to 20
+        # Now create a random order of the unicodes:
+        unicode_ord = np.random.choice(len(unicodes_list), len(unique_chars), replace=False)
+        self.mapping_dict = {c:k for c,k in zip(unique_chars, unicode_ord)}
 
-def _get_width(txt, font_size):
-    return MARGIN_WIDTH + (len(txt) * LETTER_16_WIDTH) * font_size / 16.
+        # Now "encrypt" the solutions and the secret:
+        self.solutions_enc = [[unicodes_list[self.mapping_dict[c]] for c in sol] for sol in solutions]
+        self.secret_enc = [unicodes_list[self.mapping_dict[c]] for c in secret]
 
-# Function to create a table with 6 columns and add text in the first row
-def create_table_with_columns_and_text(doc, question, ans_symbols):
-    """
-    Create a table in the doc to contain the question text, then the symbols for the solution beneath empty squares
-    :param doc: The doc created
-    :param question: The question text
-    :param ans_symbols: The answer symbols as a list of chars
-    :return: The table created
-    """
-    n_cols = len(ans_symbols) + 1
+    def _set_font_size(self, paragraphs, font_size=20):
+        for paragraph in paragraphs:
+            for run in paragraph.runs:
+                run.font.size = Pt(font_size)  # Set font size to 20
 
-    # Add a table, aligned to the right:
-    # paragraph = doc.add_paragraph()
-    # paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-    table = doc.add_table(rows=2, cols=n_cols)
-    table.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-    table.allow_autofit = True  # Disable autofit
+    def _get_width(self, txt, font_size):
+        return MARGIN_WIDTH + (len(txt) * LETTER_16_WIDTH) * font_size / 16.
 
-    # Set the width of the first five columns for a single character
-    for i in range(n_cols-1):
-        cell = table.cell(0, i)
-        cell.text = "\u25A1"
-        cell.vertical_alignment = WD_ALIGN_VERTICAL.BOTTOM  # Center align vertically
-        cell.width = Cm(1)  # Adjust the width as needed
-        _set_font_size(cell.paragraphs, SQUARE_SIZE)
+    # Function to create a table with 6 columns and add text in the first row
+    def create_table_with_columns_and_text(self, question, ans_symbols):
+        """
+        Create a table in the doc to contain the question text, then the symbols for the solution beneath empty squares
+        :param question: The question text
+        :param ans_symbols: The answer symbols as a list of chars
+        :return: The table created
+        """
+        n_cols = len(ans_symbols) + 1
 
-        cell = table.cell(1, i)
-        cell.width = Cm(1)  # Adjust the width as needed
-        cell.vertical_alignment = WD_ALIGN_VERTICAL.TOP  # Center align vertically
-        cell.text = ans_symbols[i]
-        _set_font_size(cell.paragraphs, TEXT_SIZE)
+        # Add a table, aligned to the right:
+        table = self.doc.add_table(rows=2, cols=n_cols)
+        table.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+        table.allow_autofit = True  # Disable autofit
 
-    # Set the width of the last column to fill the remaining space on the page
-    cell = table.cell(0, n_cols-1)
-    cell.width = Cm(_get_width(question, TEXT_SIZE))  # Adjust the width as needed
-    cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER  # Center align vertically
-    cell.text = question
-    _set_font_size(cell.paragraphs, TEXT_SIZE)
+        # Set the width of the first five columns for a single character
+        for i in range(n_cols-1):
+            cell = table.cell(0, i)
+            cell.text = "\u25A1"
+            cell.vertical_alignment = WD_ALIGN_VERTICAL.BOTTOM  # Center align vertically
+            cell.width = Cm(1)  # Adjust the width as needed
+            self._set_font_size(cell.paragraphs, SQUARE_SIZE)
 
-    return table
+            cell = table.cell(1, i)
+            cell.width = Cm(1)  # Adjust the width as needed
+            cell.vertical_alignment = WD_ALIGN_VERTICAL.TOP  # Center align vertically
+            cell.text = ans_symbols[i]
+            self._set_font_size(cell.paragraphs, TEXT_SIZE)
 
-# Function to add an empty paragraph for spacing
-def add_empty_paragraph(doc):
-    p = doc.add_paragraph()
-    run = p.add_run("")
-    run.font.size = Pt(1)  # Set font size to the smallest value for an empty line
+        # Set the width of the last column to fill the remaining space on the page
+        cell = table.cell(0, n_cols-1)
+        cell.width = Cm(self._get_width(question, TEXT_SIZE))  # Adjust the width as needed
+        cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER  # Center align vertically
+        cell.text = question
+        self._set_font_size(cell.paragraphs, TEXT_SIZE)
 
-# Your input sentences and symbols
-sentences = [
-    ("איזו עונה מתחילה בסוכות", "סתו"),
-    ("פרח גבוה שפורח עכשיו", "חצב"),
-    ("בונים אותה בחג סוכות", "סוכה"),
-    ("פרי עם המון גרעינים אדומים", "רימונ"),
-    ("חג סוכות נקרא גם חג", "האסיפ")
-]
-secret = "הכוכבימ"
-secret_q = " יודעים שהתחיל החג לפי"
-data = encrypt_text([s[1][::-1] for s in sentences], secret[::-1])
+        return table
 
-# Create a new Word document
-doc = Document()
+    # Function to add an empty paragraph for spacing
+    def add_empty_paragraph(self):
+        p = self.doc.add_paragraph()
+        run = p.add_run("")
+        run.font.size = Pt(1)  # Set font size to the smallest value for an empty line
 
-# Add questions with symbols to the document
-for i in range(len(sentences)):
-    question = sentences[i][0]
-    symbols = data["solutions"][i]
-    create_table_with_columns_and_text(doc, question, symbols)
-    add_empty_paragraph(doc)
-    # add_question_with_symbols(doc, question, symbols)
+    def create_doc(self, sentences, secret):
+        """
+        Create the doc from the sentences and secret.
+        :param sentences: A list of tuples of (question, answer). Answers are one word, and encrypted with the symbols
+        :param secret: A tuple of (question, answer) like the sentences
+        :return:
+        """
+        # Create the encrypted data for the sentences and secret answers:
+        self.encrypt_text([s[1][::-1] for s in sentences], secret[1][::-1])
 
-create_table_with_columns_and_text(doc, secret_q, data["secret"])
+        # Create a new Word document
+        self._new_doc()
 
-# p = doc.add_paragraph()
-# p.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
-# run = p.add_run("".join(unicodes_list))
-# run.font.size = Pt(20)
+        # Add questions with symbols to the document
+        for i in range(len(sentences)):
+            question = sentences[i][0]
+            symbols = self.solutions_enc[i]
+            self.create_table_with_columns_and_text(question, symbols)
+            self.add_empty_paragraph()
 
-# Save the document
-doc.save("/Users/talsegalov/Downloads/hebrew_questions.docx")
+        # Now add the last "secret" question:
+        self.create_table_with_columns_and_text(secret[0], self.secret_enc)
 
-print(unicodes_list)
+    def save_doc(self, filename):
+        # Save the document
+        self.doc.save(filename)
+
+
+if __name__ == '__main__':
+
+    # Your input sentences and symbols
+    sentences = [
+        ("איזו עונה מתחילה בסוכות", "סתיו"),
+        ("פרח גבוה שפורח עכשיו", "חצב"),
+        ("בונים אותה בחג סוכות", "סוכה"),
+        ("פרי עם המון גרעינים אדומים", "רימונ"),
+        ("חג סוכות נקרא גם חג", "האסיפ")
+    ]
+    secret = (" יודעים שהתחיל החג לפי", "הכוכבימ")
+    word_cipher = WordCipherGen()
+    word_cipher.create_doc(sentences, secret)
+    word_cipher.save_doc("/Users/talsegalov/Downloads/hebrew_questions.docx")
 
