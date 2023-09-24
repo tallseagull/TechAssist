3a60fb107c3218b2ac49dc9a63651b6bd39622c4
Tal Segalov <tal.segalov@anaplan.com>
2023-09-24 15:29:29 +0300

    Fixed more bugs in crossword

--- crossword_gen/crossword_gen.py
+++ crossword_gen/crossword_gen.py
@@ -369,11 +369,12 @@ class Crossword(object):
 
         for word in self.current_word_list:
             new_grid_data[word.row - 1][word.col - 1] = word.number
-            # self.set_cell(word.col, word.row, word.number)
         if self.rtl:
             new_grid = pd.DataFrame([row[::-1] for row in new_grid_data])
+            solved_grid = pd.DataFrame([row[::-1] for row in self.grid])
         else:
             new_grid = pd.DataFrame(new_grid_data)
+            solved_grid = pd.DataFrame(self.grid)
 
         if prune:
             # Columns removal:
@@ -383,6 +384,7 @@ class Crossword(object):
                     cols_to_remove.append(col)
             if cols_to_remove:
                 new_grid = new_grid.drop(columns=cols_to_remove)
+                solved_grid = solved_grid.drop(columns=cols_to_remove)
 
             # Now the rows:
             rows_to_remove = []
@@ -391,8 +393,9 @@ class Crossword(object):
                     rows_to_remove.append(row)
             if rows_to_remove:
                 new_grid = new_grid.drop(index=rows_to_remove)
+                solved_grid = solved_grid.drop(index=rows_to_remove)
 
-        return new_grid.astype(str)
+        return new_grid.astype(str), solved_grid.astype(str)
 
     def word_bank(self):
         outStr = ''
@@ -411,11 +414,10 @@ class Crossword(object):
         across_defs = []
 
         for word in self.current_word_list:
-            down_across = word.down_across()
             if word.vertical:
-                down_defs.append(f' {word.number}. {word.clue} ')
+                down_defs.append(f'{word.number}. {word.clue}')
             else:
-                across_defs.append(f' {word.number}. {word.clue} ')
+                across_defs.append(f'{word.number}. {word.clue}')
         return across_defs, down_defs
 
 
@@ -484,45 +486,22 @@ def read_word_and_defs(filename, hebrew=True):
 ### end class, start execution
 
 if __name__ == '__main__':
-
-    # word_list = ['saffron', 'The dried, orange yellow plant used to as dye and as a cooking spice.'], \
-    #             ['pumpernickel', 'Dark, sour bread made from coarse ground rye.'], \
-    #             ['leaven', 'An agent, such as yeast, that cause batter or dough to rise..'], \
-    #             ['coda', 'Musical conclusion of a movement or composition.'], \
-    #             ['paladin', 'A heroic champion or paragon of chivalry.'], \
-    #             ['syncopation', 'Shifting the emphasis of a beat to the normally weak beat.'], \
-    #             ['albatross', 'A large bird of the ocean having a hooked beek and long, narrow wings.'], \
-    #             ['harp', 'Musical instrument with 46 or more open strings played by plucking.'], \
-    #             ['piston',
-    #              'A solid cylinder or disk that fits snugly in a larger cylinder and moves under pressure as in an engine.'], \
-    #             ['caramel', 'A smooth chery candy made from suger, butter, cream or milk with flavoring.'], \
-    #             ['coral', 'A rock-like deposit of organism skeletons that make up reefs.'], \
-    #             ['dawn', 'The time of each morning at which daylight begins.'], \
-    #             ['pitch', 'A resin derived from the sap of various pine trees.'], \
-    #             ['fjord', 'A long, narrow, deep inlet of the sea between steep slopes.'], \
-    #             ['lip', 'Either of two fleshy folds surrounding the mouth.'], \
-    #             ['lime', 'The egg-shaped citrus fruit having a green coloring and acidic juice.'], \
-    #             ['mist', 'A mass of fine water droplets in the air near or in contact with the ground.'], \
-    #             ['plague', 'A widespread affliction or calamity.'], \
-    #             ['yarn', 'A strand of twisted threads or a long elaborate narrative.'], \
-    #             ['snicker', 'A snide, slightly stifled laugh.']
-
     word_list = read_word_and_defs('heb_defs.txt')
     extra_word_list = read_word_and_defs('extra_heb_defs.txt')
     heb_letters = 'אבגדהוזחטיכלמנסעפצקרשת'
 
-    a = Crossword(10, 10, '*', 5000, word_list, extra_words=extra_word_list, letters=heb_letters, rtl=True)
-    a.compute_crossword(time_permitted=5.00, spins=2)
-    print(len(a.current_word_list), 'out of', len(word_list))
+    crossword = Crossword(10, 10, '*', 5000, word_list, extra_words=extra_word_list, letters=heb_letters, rtl=True)
+    crossword.compute_crossword(time_permitted=5.00, spins=2)
+    print(len(crossword.current_word_list), 'out of', len(word_list))
 
     # Create the PDF:
+    # Create a DataFrame from the crossword data
+    empty_df, solved_df = crossword.display()
+
     # Sample definitions
-    across_defs, down_defs = a.legend()
+    across_defs, down_defs = crossword.legend()
     across_definitions = ["אופקי"] + across_defs
     down_definitions = ["מאונך"] + down_defs
-
-    # Create a DataFrame from the crossword data
-    empty_df, solved_df = a.display()
 
     # Output PDF file name
     output_filename = "crossword.pdf"
--- crossword_gen/cw_to_pdf.py
+++ crossword_gen/cw_to_pdf.py
@@ -2,7 +2,7 @@ import string
 
 from reportlab.lib.enums import TA_RIGHT
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer, Paragraph, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
 from reportlab.lib import colors
@@ -44,6 +44,50 @@ def _reverse_hebrew_text(s):
         res += nums_stack[::-1]
     return res
 
+def _gen_crossword_table(df, is_solution=False):
+    """
+    Generate a table in the PDF with the data from the grid. If this is the solution data, make the letters larger and centered
+    :return:
+    """
+    # Define cell dimensions and style
+    cell_width = 30
+    cell_height = 30
+    cell_style = getSampleStyleSheet()['Normal']
+    if is_solution:
+        cell_style.fontName = "Hebrew"
+        cell_style.fontSize = 14
+        cell_style.alignment = TA_CENTER
+    else:
+        cell_style.fontSize = 8
+        cell_style.alignment = TA_RIGHT
+
+    # Define a table to hold the crossword grid
+    data = []
+    black_cells = []
+    for i, row in enumerate(df.iterrows()):
+        row_data = []
+        for k, cell in enumerate(row[1]):
+            if cell == '*':
+                cell_content = ''
+                black_cells.append((k, i))
+            else:
+                cell_content = cell
+            row_data.append(Paragraph(cell_content, cell_style))
+        data.append(row_data)
+
+    # Create the crossword table
+    crossword_table = Table(data, colWidths=cell_width, rowHeights=cell_height)
+    cw_tab_style = TableStyle([
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('ALIGN', (0, 0), (-1, -1), 'RIGHT'),
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+    ])
+    for i, k in black_cells:
+        cw_tab_style.add('BACKGROUND', (i, k), (i, k), colors.gray)
+
+    crossword_table.setStyle(cw_tab_style)
+    return crossword_table
+
 # Function to create a crossword puzzle PDF
 def create_crossword_pdf(df, across, down, output_filename, solved_df=None):
     """
@@ -63,41 +107,7 @@ def create_crossword_pdf(df, across, down, output_filename, solved_df=None):
     # Create a list to hold the content for the PDF
     elements = []
 
-    # Define cell dimensions and style
-    cell_width = 30
-    cell_height = 30
-    cell_style = getSampleStyleSheet()['Normal']
-    cell_style.fontSize = 8
-    cell_style.alignment = TA_RIGHT
-
-    # Define a table to hold the crossword grid
-    data = []
-    black_cells = []
-    for i,row in enumerate(df.iterrows()):
-        row_data = []
-        for k,cell in enumerate(row[1]):
-            if cell == '*':
-                cell_content = '*'
-                black_cells.append((k, i))
-            elif cell == ' ':
-                cell_content = ''
-            else:
-                cell_content = cell
-            row_data.append(Paragraph(cell_content, cell_style))
-        data.append(row_data)
-
-    # Create the crossword table
-    crossword_table = Table(data, colWidths=cell_width, rowHeights=cell_height)
-    cw_tab_style = TableStyle([
-        ('GRID', (0, 0), (-1, -1), 1, colors.black),
-        ('ALIGN', (0, 0), (-1, -1), 'RIGHT'),
-        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-    ])
-    for i,k in black_cells:
-        cw_tab_style.add('BACKGROUND', (i,k), (i,k), colors.black)
-
-    crossword_table.setStyle(cw_tab_style)
-
+    crossword_table = _gen_crossword_table(df)
     elements.append(crossword_table)
     elements.append(Spacer(1, 20))  # Spacer for separation
 
@@ -109,6 +119,12 @@ def create_crossword_pdf(df, across, down, output_filename, solved_df=None):
     elements.append(Spacer(1, 20))  # Spacer for separation
     elements.append(
         Paragraph(f"<strong><u>{down[0][::-1]}</u></strong><br/>" + "<br/>".join([_reverse_hebrew_text(a) for a in down[1:]]), heb_style))
+
+    # If we have the solution grid, add it as well with a page break:
+    if solved_df is not None:
+        elements.append(PageBreak())
+        solution_table = _gen_crossword_table(solved_df, is_solution=True)
+        elements.append(solution_table)
 
     # Build the PDF document
     doc.build(elements)
